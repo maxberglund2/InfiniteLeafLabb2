a11363245df2ed3ab48b8d3aad9af4dec21c2d5f
Greed <106820018+maxberglund2@users.noreply.github.com>
2026-01-29 11:44:53 +0100

    menu page NOT DONE

---
+++ InfiniteLeafLabb2/Controllers/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using InfiniteLeafLabb2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfiniteLeafLabb2.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ILogger<MenuController> _logger;
+        private readonly MenuService _menuService;
+
+        public MenuController(ILogger<MenuController> logger, MenuService menuService)
+        {
+            _logger = logger;
+            _menuService = menuService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _menuService.GetAllMenuItemsAsync();
+            ViewBag.MenuItems = menuItems ?? new List<InfiniteLeafLabb2.Models.DTOs.MenuItemDto>();
+
+            return View();
+        }
+    }
+}
